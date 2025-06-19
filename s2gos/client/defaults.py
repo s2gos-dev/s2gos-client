3774bc8aff98f804ffef69592bb58865cc876b64
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-06-19 07:35:18 +0200

    using 127.0.0.1 instead of localhost

--- s2gos/client/defaults.py
+++ s2gos/client/defaults.py
@@ -11,4 +11,4 @@ DEFAULT_USER_PATH: Final = Path("~").expanduser() / ".s2gos"
 DEFAULT_CONFIG_PATH: Final = DEFAULT_USER_PATH / "config"
 
 DEFAULT_REQUEST_FILE: Final = "s2gos-request.yaml"
-DEFAULT_SERVER_URL: Final = "http://localhost:8008"
+DEFAULT_SERVER_URL: Final = "http://127.0.0.1:8008"
